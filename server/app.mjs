c8953b1f433b80c273c2e31974ada1185bba4be0
atulnagose149 <nagoseatul149@gmail.com>
2024-08-08 10:22:58 +0530

    Production to Development

--- server/app.mjs
+++ server/app.mjs
@@ -185,7 +185,7 @@ const MONGODB_URI =
     : process.env.DEV_MONGODB_URI;
 
 const numOfCPU = os.availableParallelism();
-
+console.log(MONGODB_URI);
 if (cluster.isPrimary) {
   for (let i = 0; i < numOfCPU; i++) {
     cluster.fork();
